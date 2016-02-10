03ff6f4f4b303aac000f00a11b75e518787f0f4c
satoyami <satoyami@yahoo.com>
2016-02-09 20:04:37 -0800

    simple API returning json data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mongoserve
 test mongodb with node
+## Local Mongodb instance setup
+- test is to perform query on DB
+- also incorporate Mocha unit testing
+
--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var url = 'mongodb://10.0.0.44:27017/test';
+
+var getmongo = function(db,score,callback) {
+  var results = [];
+  var query = {"address.zipcode":"10075","grades.score":{$gt:score}}; 
+  console.log(query);
+  var cursor = db.collection('restaurants').find(query );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if(doc != null) {
+        results.push(doc);
+      }else {
+        //console.log(results);
+        callback(results);
+        db.close();
+      }
+   });
+};
+
+
+
+app.get('/:score', function (req, res) {
+  var gs = parseInt(req.params.score,10);
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+
+    getmongo(db,gs,function(docs){
+      res.json(docs);
+    });
+  });
 });
 
 app.listen(3000, function () {
