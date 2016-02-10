e0716636712351dce703972851dd164ffffb660b
satoyami <satoyami@yahoo.com>
2016-02-09 21:34:15 -0800

    added score and grade API

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ var app = express();
 
 var url = 'mongodb://10.0.0.44:27017/test';
 
-var getmongo = function(db,score,callback) {
+/*
+ * Query function defintion
+ */
+
+var restScore = function(db,score,callback) {
   var results = [];
   var query = {"grades.score":{$gt:score}}; 
   console.log(query);
-  var cursor = db.collection('restaurants').find(query,{"name":1});
+  var cursor = db.collection('restaurants').find(query);
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if(doc != null) {
+        results.push(doc);
+      }else {
+        //console.log(results);
+        callback(results);
+        db.close();
+      }
+   });
+};
+var restGrade = function(db,grade,callback) {
+  var results = [];
+  var query = {"grades.grade":grade}; 
+  console.log(query);
+  var cursor = db.collection('restaurants').find(query);
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if(doc != null) {
@@ -22,16 +42,34 @@ var getmongo = function(db,score,callback) {
    });
 };
 
-app.get('/:score', function (req, res) {
+/*
+ * API defintion
+ */
+
+app.get('/score/:score', function (req, res) {
   var gs = parseInt(req.params.score,10);
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
 
-    getmongo(db,gs,function(docs){
+    restScore(db,gs,function(docs){
       res.json(docs);
     });
   });
 });
+app.get('/grade/:grade', function (req, res) {
+  var g = req.params.grade;
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+
+    restGrade(db,g,function(docs){
+      res.json(docs);
+    });
+  });
+});
+
+/*
+ * Server defintion
+ */
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
